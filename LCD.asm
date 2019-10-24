aa72665bedb80cd3a6ce7992cd4d92844814c2ec
sjy115 <sjy115@skbl-422-006.ph.ic.ac.uk>
2019-10-24 17:08:55 +0100

    Keypad KOMPLETE but then displaying 2nd row in LCD takes long

--- LCD.asm
+++ LCD.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message, LCD_clear_display, LCD_set_position
+    global  LCD_Setup, LCD_Write_Message, LCD_clear_display, LCD_set_position, LCD_Send_Byte_D, LCD_delay_ms
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
